a0195edcd1a2e9e6edd23be0eb5fae68efa8906f
Khang Nguyen Phuc <khangnguyen261102@gmail.com>
2024-04-11 20:16:30 +0700

    [update] get tours - remove formatted

--- src/api/controllers/tour.controller.js
+++ src/api/controllers/tour.controller.js
@@ -1,6 +1,5 @@
 import HttpStatus from 'http-status';
 import Tour from '../models/tour.model.js';
-import Station from '../models/station.model.js';
 import APIError from '../errors/api-error.js';
 /**
  * Get user list
@@ -8,31 +7,8 @@ import APIError from '../errors/api-error.js';
  */
 export const list = async (req, res, next) => {
   try {
-    const [tours, stations] = await Promise.all([
-      Tour.list(req.query, req.user._id),
-      Station.list({}),
-    ]);
-    const formattedTours = tours.map((tour) => {
-      const fromStation = stations.find((station) => station.stationId === tour.fromStation);
-      const toStation = stations.find((station) => station.stationId === tour.toStation);
-      const item = {
-        _id: tour._id,
-        status: tour.status,
-        userId: tour.userId,
-        fromStation: {
-          stationId: tour.fromStation,
-          name: fromStation.name,
-        },
-        toStation: {
-          stationId: tour.toStation,
-          name: toStation.name,
-        },
-        createdAt: tour.createdAt,
-        updatedAt: tour.updatedAt,
-      };
-      return item;
-    });
-    res.json(formattedTours);
+    const tours = await Tour.list(req.query, req.user._id);
+    res.json(tours);
   } catch (error) {
     next(error);
   }
@@ -67,31 +43,27 @@ export const updateStatus = async (req, res, next) => {
 
 export const getCurrent = async (req, res, next) => {
   try {
-    const [lastestTourArr, stations] = await Promise.all([
-      Tour.list({ perpage: 1 }, req.user._id),
-      Station.list({}),
-    ]);
+    const lastestTourArr = await Tour.list({ perpage: 1 }, req.user._id);
     const [latestTour] = lastestTourArr;
     if (latestTour.status === 'picking' || latestTour.status === 'leading') {
-      const fromStation = stations.find((station) => station.stationId === latestTour.fromStation);
-      // const toStation = stations.find((station) => station.stationId === latestTour.toStation);
       const formattedTour = {
         _id: latestTour._id,
         status: latestTour.status,
         userId: latestTour.userId,
         lastStation: latestTour.lastStation,
-        fromStation: {
-          stationId: latestTour.fromStation,
-          name: fromStation.name,
-          location: fromStation.location,
-          description: fromStation.description,
-        },
+        // fromStation: {
+        //   stationId: latestTour.fromStation,
+        //   name: fromStation.name,
+        //   location: fromStation.location,
+        //   description: fromStation.description,
+        // },
         // toStation: {
         //   stationId: latestTour.toStation,
         //   name: toStation.name,
         //   location: toStation.location,
         //   description: toStation.description,
         // },
+        fromStation: lastestTourArr.fromStation,
         toStation: latestTour.toStation,
         createdAt: latestTour.createdAt,
         updatedAt: latestTour.updatedAt,
